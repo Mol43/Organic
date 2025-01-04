b9c6833bd7d0fef5a821f2ae3bca0aa14386a7bb
jorabekhub <lenovopromax.222@gmail.com>
2025-01-04 18:36:08 +0500

    update logoheader

--- src/Components/common/LogoHeader.jsx
+++ src/Components/common/LogoHeader.jsx
@@ -3,7 +3,7 @@
 
 function LogoHeader( { bgImg , title }) {
     return (
-      <div className={`w-full h-[350px] flex items-center justify-center relative`}>
+      <div className={`w-full h-[350px] flex items-center justify-center relative mt-4`}>
           <img src={bgImg} alt="img" className="w-full h-full absolute top-0 left-0" />
           <h2 className="text-[70px] text-[#274C5B] z-10 font-bold">{title}</h2>
       </div>
